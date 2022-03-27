613dfec9119ee0d303dab14b2abbafbdf6a82fd6
leinad-kobko <danielkobko@gmail.com>
2022-03-26 23:01:22 -0400

    changed profile image

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import Layout from "../components/Layout";
 import Head from "next/head";
 import Image from "next/image";
-import me from '../images/me2.png'
+import me from '../icons/Profile.png'
 import github from '../icons/github.png'
 import { DiGithubBadge } from 'react-icons/di';
 import { BsLinkedin } from 'react-icons/bs';
@@ -21,15 +21,39 @@ export default function Home() {
                 id="content-wrapper" 
                 className="h-full flex-col flex content-stretch lg:justify-between lg:flex-row items-center"
             >
-                <div id="image-container" className="flex md:w-1/2 justify-center">
-                    <Image
-                        alt="me.png"
-                        src={me}
-                        layout="intrinsic"
-                        width={900*0.3}
-                        height={900*0.3}
-                        className="object-contain justify-center"
-                    />
+                <div id="card-area" className="flex justify-center w-1/2">
+                    <div id="me-card" className="w-64 h-72 my-10">
+                        <div id="imageArea" className="h-5/6 bg-gradient-to-bl from-blue-100 to-red-100 flex flex-col justify-center">
+                            <Image
+                                alt="me.png"
+                                src={me}
+                                layout="intrinsic"
+                                width={900*0.15}
+                                height={900*0.15}
+                                border="2px solid black"
+                                className="object-contain"
+                            />
+                            <p className="text-xl lg:text-2xl font-bold text-red-400 text-center">Software Developer</p>
+                        </div>
+                        <div id="links" className="h-1/6 bg-white flex">
+                            <div 
+                                id="link-buttons"
+                                className="flex items-center justify-center w-full space-x-10"
+                            >
+                                <div id="github-button">
+                                    <LinkButton href="https://github.com/leinad-kobko">
+                                        <DiGithubBadge size="40" color={secondary_colour} className={buttoneffects}/>
+                                    </LinkButton>
+                                </div>
+                                <LinkButton href="https://www.linkedin.com/in/daniel-kobko-19a785176/">
+                                    <BsLinkedin size="30" color={secondary_colour} className={buttoneffects}/>
+                                </LinkButton>
+                                <LinkButton href="mailto:danielkobko@gmail.com">
+                                    <HiMail size="40" color={secondary_colour} className={buttoneffects}/>
+                                </LinkButton>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div id="content" className="flex flex-col md:w-1/2 items-center">
                     <div>
@@ -38,22 +62,7 @@ export default function Home() {
                             <p className="text-4xl lg:text-5xl font-bold text-transparent bg-clip-text bg-gradient-to-bl from-blue-400 to-red-500">{"I'm Daniel Kobko"}</p>
                         </div>
                         <div className="flex justify-center py-3">
-                            <div 
-                                id="link-buttons"
-                                className="flex items-center justify-between w-4/5"
-                            >
-                                <div id="github-button">
-                                    <LinkButton href="https://github.com/leinad-kobko">
-                                        <DiGithubBadge size="70" color={secondary_colour} className={buttoneffects}/>
-                                    </LinkButton>
-                                </div>
-                                <LinkButton href="https://www.linkedin.com/in/daniel-kobko-19a785176/">
-                                    <BsLinkedin size="50" color={secondary_colour} className={buttoneffects}/>
-                                </LinkButton>
-                                <LinkButton href="mailto:danielkobko@gmail.com">
-                                    <HiMail size="60" color={secondary_colour} className={buttoneffects}/>
-                                </LinkButton>
-                            </div>
+                            
                         </div>
                     </div>
                 </div>
